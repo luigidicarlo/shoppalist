b0cccd7309694f6709747eae8a01665279b3ed1c
Luis Huerta <lehuertad95@gmail.com>
2021-10-01 20:06:06 -0400

    New version of Shoppalist with improved features

--- src/App.js
+++ src/App.js
@@ -2,156 +2,132 @@ import { useEffect, useState } from 'react';
 import swal from 'sweetalert';
 import { v4 } from 'uuid';
 
-import Items from './Items';
-import Header from './Header';
+import { Items } from './Items';
+import { Header } from './Header';
+import { Form } from './Form';
+import { FilterForm } from './FilterForm';
 
 const defaultState = {
-	name: '',
-	quantity: '',
-	price: '',
-	itemToEdit: null,
+  name: '',
+  quantity: '',
+  price: '',
+  itemToEdit: null,
 };
 
-export default function App() {
-	const rawItems = localStorage.getItem('items');
-	const defaultItems = rawItems ? JSON.parse(rawItems) : [];
-	const [items, setItems] = useState(defaultItems);
-	const [formState, setFormState] = useState(defaultState);
-	const [showForm, setShowForm] = useState(false);
+export const App = () => {
+  const rawItems = localStorage.getItem('items');
+  const defaultItems = rawItems ? JSON.parse(rawItems) : [];
+  const [items, setItems] = useState(defaultItems);
+  const [filteredItems, setFilteredItems] = useState(items);
+  const [filter, setFilter] = useState('');
+  const [formState, setFormState] = useState(defaultState);
+  const [showForm, setShowForm] = useState(false);
 
-	useEffect(() => {
-		localStorage.setItem('items', JSON.stringify(items));
-	}, [items]);
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
 
-	function onSubmit(e) {
-		e.preventDefault();
+  useEffect(() => {
+    if (filter.length > 0) {
+      const filteredElements = items.filter(item =>
+        item.name.toLowerCase().includes(filter.toLowerCase())
+      );
+      setFilteredItems(filteredElements);
+    } else {
+      setFilteredItems(items);
+    }
+  }, [filter, items]);
 
-		const { name, quantity, price, itemToEdit } = formState;
+  const onSubmit = e => {
+    e.preventDefault();
 
-		if (formState.itemToEdit) {
-			updateItem(itemToEdit.id, {
-				id: itemToEdit.id,
-				name: name.trim(),
-				quantity: Number(quantity),
-				price: Number(price),
-			});
-		} else {
-			const newItem = {
-				id: v4(),
-				name: name.trim(),
-				quantity: Number(quantity),
-				price: Number(price),
-			};
+    const { name, quantity, price, itemToEdit } = formState;
 
-			setItems([newItem, ...items]);
-		}
+    if (formState.itemToEdit) {
+      updateItem(itemToEdit.id, {
+        id: itemToEdit.id,
+        name: name.trim(),
+        quantity: Number(quantity),
+        price: Number(price),
+      });
+    } else {
+      const newItem = {
+        id: v4(),
+        name: name.trim(),
+        quantity: Number(quantity),
+        price: Number(price),
+      };
 
-		resetForm();
-	}
+      setItems([newItem, ...items]);
+    }
 
-	function onChange({ target }) {
-		setFormState(state => ({ ...state, [target.name]: target.value }));
-	}
+    resetForm();
+  };
 
-	function onToggleForm() {
-		setShowForm(show => !show);
-	}
+  const onChange = ({ target }) => {
+    setFormState(state => ({ ...state, [target.name]: target.value }));
+  };
 
-	function resetForm() {
-		setFormState(defaultState);
-	}
+  const onToggleForm = () => {
+    setShowForm(show => !show);
+  };
 
-	function updateItem(itemId, itemData) {
-		setItems(items.map(item => (item.id === itemId ? itemData : item)));
-	}
+  const resetForm = () => {
+    setFormState(defaultState);
+  };
 
-	function deleteItem(itemId) {
-		swal({
-			title: 'Eliminar',
-			text: '¿Deseas eliminar el elemento seleccionado?',
-			buttons: ['Cancelar', 'Eliminar'],
-			dangerMode: true,
-		}).then(confirmed => {
-			if (!confirmed) return;
+  const updateItem = (itemId, itemData) => {
+    setItems(items.map(item => (item.id === itemId ? itemData : item)));
+  };
 
-			setItems(items => items.filter(item => item.id !== itemId));
-		});
-	}
+  const deleteItem = itemId => {
+    swal({
+      title: 'Eliminar',
+      text: '¿Deseas eliminar el elemento seleccionado?',
+      buttons: ['Cancelar', 'Eliminar'],
+      dangerMode: true,
+    }).then(confirmed => {
+      if (!confirmed) return;
 
-	return (
-		<>
-			<Header />
-			<div className="container">
-				<div className="card mb-4">
-					<div className="card-header">
-						<div className="d-flex align-items-center justify-content-center">
-							<button
-								className="btn btn-sm btn-secondary text-white"
-								type="button"
-								onClick={onToggleForm}
-							>
-								{showForm ? 'Ocultar Formulario' : 'Mostrar Formulario'}
-							</button>
-						</div>
-					</div>
-					{showForm && (
-						<div className="card-body">
-							<form
-								onSubmit={onSubmit}
-								className="form-inline d-flex flex-sm-row flex-column align-items-center justify-content-between"
-							>
-								<div className="form-group mb-2 me-1">
-									<input
-										className="form-control"
-										type="text"
-										name="name"
-										value={formState.name}
-										onChange={onChange}
-										placeholder="Nombre"
-									/>
-								</div>
-								<div className="form-group mb-2 me-1">
-									<input
-										type="number"
-										min="0"
-										step="0.01"
-										name="quantity"
-										className="form-control"
-										value={formState.quantity}
-										onChange={onChange}
-										placeholder="Cantidad (ejemplo: 5 ó 2.5)"
-									/>
-								</div>
-								<div className="form-group mb-2 me-1">
-									<input
-										type="number"
-										min="0"
-										step="0.01"
-										name="price"
-										className="form-control"
-										value={formState.price}
-										onChange={onChange}
-										placeholder="Precio (ejemplo: 9.99)"
-									/>
-								</div>
-								<button className="d-block btn btn-success mb-2" type="submit">
-									{formState.itemToEdit ? 'Editar' : 'Agregar'}
-								</button>
-							</form>
-						</div>
-					)}
-				</div>
-				<div className="row">
-					<div className="col-xs-12">
-						<Items
-							items={items}
-							deleteItem={deleteItem}
-							setFormState={setFormState}
-							setShowForm={setShowForm}
-						/>
-					</div>
-				</div>
-			</div>
-		</>
-	);
-}
+      setItems(items => items.filter(item => item.id !== itemId));
+    });
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="card mb-4">
+          <div className="card-header bg-white">
+            <div className="d-flex align-items-center justify-content-center">
+              <button
+                className="btn btn-sm btn-secondary text-white"
+                type="button"
+                onClick={onToggleForm}
+              >
+                {showForm ? 'Ocultar Formulario' : 'Mostrar Formulario'}
+              </button>
+            </div>
+          </div>
+          {showForm && (
+            <div className="card-body">
+              <Form
+                onSubmit={onSubmit}
+                formState={formState}
+                onChange={onChange}
+                resetForm={resetForm}
+              />
+            </div>
+          )}
+        </div>
+        <FilterForm filter={filter} setFilter={setFilter} />
+        <Items
+          items={filteredItems}
+          deleteItem={deleteItem}
+          setFormState={setFormState}
+          setShowForm={setShowForm}
+        />
+      </div>
+    </>
+  );
+};
--- src/Items.js
+++ src/Items.js
@@ -1,86 +1,49 @@
 import React from 'react';
+import { Item } from './Item';
 
-export default function Items({
-	items,
-	deleteItem,
-	setFormState,
-	setShowForm,
-}) {
-	function getSubtotal(item) {
-		return Number(item.quantity) * Number(item.price);
-	}
+export const Items = ({ items, deleteItem, setFormState, setShowForm }) => {
+  const getSubtotal = item => {
+    return Number(item.quantity) * Number(item.price);
+  };
 
-	function getTotal() {
-		return items.reduce((prev, curr) => Number((prev += getSubtotal(curr))), 0);
-	}
+  const getTotal = () => {
+    return items.reduce((prev, curr) => Number((prev += getSubtotal(curr))), 0);
+  };
 
-	function onEdit(item) {
-		setShowForm(true);
-		setFormState({
-			name: item.name,
-			quantity: item.quantity,
-			price: item.price,
-			itemToEdit: item,
-		});
-	}
+  const onEdit = item => {
+    setShowForm(true);
+    setFormState({
+      name: item.name,
+      quantity: item.quantity,
+      price: item.price,
+      itemToEdit: item,
+    });
+  };
 
-	return (
-		<>
-			<div className="d-flex flex-md-row flex-column justify-content-md-between px-md-5">
-				<p className="text-center">
-					<i className="fas fa-info me-2 text-info" /> Haz clic/tap sobre una
-					fila para editarla
-				</p>
-				<p className="text-center">
-					<i className="fas fa-info me-2 text-info" /> Haz doble clic/tap sobre
-					una fila para eliminarla
-				</p>
-			</div>
-			<div className="table-responsive">
-				<table className="table table-bordered">
-					<thead className="bg-primary text-white">
-						<tr>
-							<th>Nombre</th>
-							<th>Cantidad</th>
-							<th>Precio</th>
-							<th>Subtotal</th>
-						</tr>
-					</thead>
-					<tbody>
-						{!items.length && (
-							<tr>
-								<td colSpan={4}>
-									<h4 className="text-center">No hay elementos en la lista</h4>
-								</td>
-							</tr>
-						)}
-						{items.length > 0 &&
-							items.map(item => (
-								<tr
-									key={item.id}
-									onClick={() => onEdit(item)}
-									onDoubleClick={() => deleteItem(item.id)}
-								>
-									<td>{item.name}</td>
-									<td>{Number(item.quantity).toFixed(2)}</td>
-									<td>{Number(item.price).toFixed(2)}</td>
-									<td>{getSubtotal(item).toFixed(2)}</td>
-								</tr>
-							))}
-					</tbody>
-					{items.length > 0 && (
-						<tfoot>
-							<tr>
-								<td colSpan={4}>
-									<h5 className="text-center">
-										Total: {Number(getTotal()).toFixed(2)}
-									</h5>
-								</td>
-							</tr>
-						</tfoot>
-					)}
-				</table>
-			</div>
-		</>
-	);
-}
+  return (
+    <ul className="list-group shadow-sm">
+      {!items.length && (
+        <li className="list-group-item">
+          <h2 className="text-center text-secondary">
+            No hay elementos para mostrar
+          </h2>
+        </li>
+      )}
+      {items.length > 0 && (
+        <li className="list-group-item">
+          <h2 className="text-center my-0">{Number(getTotal()).toFixed(3)}</h2>
+          <p className="text-secondary text-center small">Total</p>
+        </li>
+      )}
+      {items.length > 0 &&
+        items.map(item => (
+          <Item
+            item={item}
+            key={item.id}
+            onEdit={onEdit}
+            onDelete={deleteItem}
+          />
+        ))}
+    </ul>
+  );
+};
